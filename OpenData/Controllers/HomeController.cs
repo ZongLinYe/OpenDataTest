3fd119026e47c158d5fd11b3e4e10d4999a480de
ZongLinYe <bluewingleaf@livemai.tw>
2023-09-12 23:40:19 +0800

    加入專案檔案。

---
+++ OpenData/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenData.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace OpenData.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+
+            return View();
+        }
+
+        public async Task<IActionResult> DisplayParkInTaipei()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://tppkl.blob.core.windows.net/blobfs/TaipeiParkFacility_Arcade.json"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //var result = JsonConvert.DeserializeObject<List<OpenData.Models.ViewModel>>(apiResponse);
+                    var parkList = JsonSerializer.Deserialize<List<Park>>(apiResponse);
+
+                    var result = parkList.Select((x,index) =>
+                    {
+                        x.Index = index;
+                        x.JSON = JsonSerializer.Serialize(x);
+                        return x;
+                    });
+
+                    return View(result);
+                }
+            }
+        }
+
+        /*DeleteParkInTaipei?jsonData=${jsonDataString}&id=${id}*/
+        [HttpDelete]
+        public ActionResult DeleteParkInTaipei(string jsonData,int id)
+        {
+            var a = JsonSerializer.Deserialize<Park>(jsonData);
+            var idIndex = id;
+            return NoContent();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OpenData/Models/test.cs
@@ -0,0 +1,29 @@
+﻿namespace OpenData.Models
+{
+    public class ViewModel
+    {
+        public List<Park> ParkList { get; set; }
+    }
+
+    public class Park
+    {
+        public int Index { get; set; }
+        public string 公園名稱 { get; set; }
+        public string 行政區 { get; set; }
+        public string 管理單位 { get; set; }
+        public string 組合遊具數量 { get; set; }
+        public string 磨石滑梯數量 { get; set; }
+        public string 鞦韆數量 { get; set; }
+        public string 翹翹板數量 { get; set; }
+        public string 搖搖樂數量 { get; set; }
+        public string 攀爬組數量 { get; set; }
+        public string 戲沙坑數量 { get; set; }
+        public string 擺盪船索數量 { get; set; }
+        public string 其他數量 { get; set; }
+        public string X坐標 { get; set; }
+        public string Y坐標 { get; set; }
+        public string JSON { get; set; }
+    }
+
+
+}
